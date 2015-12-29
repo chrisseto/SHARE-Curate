ce2b5410c5c3d6374bfb7d6b086e17a6daa8b661
Chris Seto <chriskseto@gmail.com>
2015-12-28 23:33:31 -0500

    Update URIs

--- app/adapters/author.js
+++ app/adapters/author.js
@@ -3,6 +3,6 @@ import UrlTemplates from 'ember-data-url-templates';
 
 export default DS.JSONAPIAdapter.extend(UrlTemplates, {
     host: 'http://localhost:1212',
-    urlTemplate: '{+host}/v1/namespaces/SHARE/collections/authors/documents{/id}',
-    queryUrlTemplate: '{+host}/v1/namespaces/SHARE/collections/authors/_search',
+    urlTemplate: '{+host}/v1/namespaces/SHARE/collections/share-contributor/documents{/id}',
+    queryUrlTemplate: '{+host}/v1/namespaces/SHARE/collections/share-contributor/_search',
 });
--- app/adapters/normalized.js
+++ app/adapters/normalized.js
@@ -3,6 +3,6 @@ import UrlTemplates from 'ember-data-url-templates';
 
 export default DS.JSONAPIAdapter.extend(UrlTemplates, {
     host: 'http://localhost:1212',
-    urlTemplate: '{+host}/v1/namespaces/SHARE/collections/normalized/documents{/id}',
-    queryUrlTemplate: '{+host}/v1/namespaces/SHARE/collections/normalized/_search',
+    urlTemplate: '{+host}/v1/namespaces/SHARE/collections/share-data/documents{/id}',
+    queryUrlTemplate: '{+host}/v1/namespaces/SHARE/collections/share-data/_search',
 });
